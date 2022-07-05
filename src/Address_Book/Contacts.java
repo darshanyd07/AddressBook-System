b61d924e69dbd19ee2cd10c0b73ed7a20c94ffd5
champ09D <darshanmhatre709@gmail.com>
2022-07-05 17:33:29 +0530

    Edit UC File Done

--- src/Address_Book/Contacts.java
+++ src/Address_Book/Contacts.java
@@ -1,119 +1,22 @@
 package Address_Book;
 import java.util.*;
- public class Contacts
- {
-     static Scanner scanner = new Scanner(System.in);
-     private String firstName;
-     private String lastName;
-     private String email;
-     private String phoneNumber;
-     private String zip;
-     private String city;
-     private String state;
-     private String address;
+class contacts
+{
+    String firstname;
+    String lastname;
+    String city;
+    String phoneNumber;
+    public contacts(String firstname,String lastname,String city,String phoneNumber)
+    {
+        System.out.println("The Contact Details are as follows");
+        this.firstname=firstname;
+        this.lastname=lastname;
+        this.city=city;
+        this.phoneNumber=phoneNumber;
 
-     public String getFirstName() {
-         return firstName;
-     }
+        System.out.println("Name of the contact is : "+this.firstname+" "+this.lastname);
+        System.out.println("City is "+this.city);
+        System.out.println("Phone number= "+this.phoneNumber);
 
-     public void setFirstName(String firstName) {
-         this.firstName = firstName;
-     }
-
-     public String getLastName() {
-         return lastName;
-     }
-
-     public void setLastName(String lastName) {
-         this.lastName = lastName;
-     }
-
-     public String getEmail() {
-         return email;
-     }
-
-     public void setEmail(String email) {
-         this.email = email;
-     }
-
-     public String getPhoneNumber() {
-         return phoneNumber;
-     }
-
-     public void setPhoneNumber(String phoneNumber) {
-         this.phoneNumber = phoneNumber;
-     }
-
-     public String getZip() {
-         return zip;
-     }
-
-     public void setZip(String zip) {
-         this.zip = zip;
-     }
-
-     public String getCity() {
-         return city;
-     }
-
-     public void setCity(String city) {
-         this.city = city;
-     }
-
-     public String getState() {
-         return state;
-     }
-
-     public void setState(String state) {
-         this.state = state;
-     }
-
-     public String getAddress() {
-         return address;
-     }
-
-     public void setAddress(String address) {
-         this.address = address;
-     }
-
-     public void editContact(){
-         System.out.println("Enter the firstname to ne  replaced");
-         String firstName = scanner.next();
-         setFirstName(firstName);
-         System.out.println("Enter LastName");
-         String lastName = scanner.next();
-         setLastName(lastName);
-         System.out.println("Enter Email");
-         String email = scanner.next();
-         setEmail(email);
-         System.out.println("Enter Phn No.");
-         String phoneNumber = scanner.next();
-         setPhoneNumber(phoneNumber);
-         System.out.println("Enter Zip");
-         String zip = scanner.next();
-         setZip(zip);
-         System.out.println("Enter City");
-         String city = scanner.next();
-         setCity(city);
-         System.out.println("Enter State");
-         String state = scanner.next();
-         setState(state);
-         System.out.println("Enter Address");
-         String address = scanner.next();
-         setAddress(address);
-     }
-
-     @Override
-     public String toString() {
-         return "Contacts{" +
-                 "firstName='" + firstName + '\'' +
-                 ", lastName='" + lastName + '\'' +
-                 ", email='" + email + '\'' +
-                 ", phoneNumber='" + phoneNumber + '\'' +
-                 ", zip='" + zip + '\'' +
-                 ", city='" + city + '\'' +
-                 ", state='" + state + '\'' +
-                 ", address='" + address + '\'' +
-                 '}';
-     }
- }
+    }
+}
