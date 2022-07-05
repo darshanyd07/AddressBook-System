94552171ae8155d5f9f92ebadcb5a59eb0214072
champ09D <darshanmhatre709@gmail.com>
2022-07-05 16:23:38 +0530

    UC1 File Done

---
+++ src/Address_Book/AddressBook.java
@@ -0,0 +1,40 @@
+package Address_Book;
+import java.util.Scanner;
+public class AddressBook
+{
+    static Scanner sc=new Scanner(System.in);
+    public void addContacts() {
+        Contacts contact = new Contacts();
+        System.out.println("enter the first name");
+        String firstName=sc.next();
+        contact.setFirstName(firstName);
+        System.out.println("enter the last name");
+        String lastName=sc.next();
+        contact.setLastName(lastName);
+        System.out.println("enter the email");
+        String email=sc.next();
+        contact.setEmail(email);
+        System.out.println("enter the phone number");
+        String phoneNumber=sc.next();
+        contact.setPhoneNumber(phoneNumber);
+        System.out.println("enter the zip code");
+        String zip=sc.next();
+        contact.setZip(zip);
+        System.out.println("enter the city name");
+        String city=sc.next();
+        contact.setCity(city);
+        System.out.println("enter the name of the state");
+        String state=sc.next();
+        contact.setState(state);
+        System.out.println("enter the address");
+        String address=sc.next();
+        contact.setAddress(address);
+
+        System.out.println(contact.toString());
+    }
+    public static void main(String[] args) {
+        System.out.println("------Welcome to Address Book---------");
+        AddressBook addressbook=new AddressBook();
+        addressbook.addContacts();
+    }
+}
--- src/Address_Book/AddressBook_System.java
+++
@@ -1,9 +0,0 @@
-package Address_Book;
-public class AddressBook_System
-{
-    public static void main(String[] darsh)
-
-    {
-            System.out.println("-----Welcome to Address Book Program-----");
-    }
-}
